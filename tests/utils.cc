57dab6d709e15fb39ee6e60de4d3586dc3d0e26c
Ricardo Montañana <rmontanana@gmail.com>
2023-07-18 13:43:26 +0200

    Add some tests

---
+++ tests/utils.cc
@@ -0,0 +1,3 @@
+//
+// Created by Ricardo Montañana Gómez on 18/7/23.
+//
---
+++ tests/utils.h
@@ -0,0 +1,8 @@
+//
+// Created by Ricardo Montañana Gómez on 18/7/23.
+//
+
+#ifndef BAYESNET_UTILS_H
+#define BAYESNET_UTILS_H
+
+#endif //BAYESNET_UTILS_H
