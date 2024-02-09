2163e95c4afb5a4a5943cc5e71ead9061f6d39c0
Ricardo Montañana Gómez <rmontanana@gmail.com>
2024-02-09 10:57:19 +0100

    add getNotes method

--- src/BayesNet/BaseClassifier.h
+++ src/BayesNet/BaseClassifier.h
@@ -26,6 +26,7 @@ namespace bayesnet {
         std::vector<std::string> virtual graph(const std::string& title = "") const = 0;
         virtual std::string getVersion() = 0;
         std::vector<std::string> virtual topological_order() = 0;
+        std::vector<std::string> virtual getNotes() const = 0;
         void virtual dump_cpt()const = 0;
         virtual void setHyperparameters(const nlohmann::json& hyperparameters) = 0;
         std::vector<std::string>& getValidHyperparameters() { return validHyperparameters; }
--- src/BayesNet/Classifier.h
+++ src/BayesNet/Classifier.h
@@ -43,6 +43,7 @@ namespace bayesnet {
         float score(std::vector<std::vector<int>>& X, std::vector<int>& y) override;
         std::vector<std::string> show() const override;
         std::vector<std::string> topological_order()  override;
+        std::vector<std::string> getNotes() const override { return notes; }
         void dump_cpt() const override;
         void setHyperparameters(const nlohmann::json& hyperparameters) override; //For classifiers that don't have hyperparameters
     };
