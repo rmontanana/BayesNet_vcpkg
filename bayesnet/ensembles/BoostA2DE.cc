80043d5181355739b80e88097fc1d9f2f8ea8ba0
Ricardo Montañana Gómez <rmontanana@gmail.com>
2024-05-16 14:32:59 +0200

    First approach to BoostA2DE::trainModel

--- bayesnet/ensembles/BoostA2DE.cc
+++ bayesnet/ensembles/BoostA2DE.cc
@@ -81,89 +81,84 @@ namespace bayesnet {
         // run out of features
         bool ascending = order_algorithm == Orders.ASC;
         std::mt19937 g{ 173 };
+        std::vector<std::pair<int, int>> pairSelection;
         while (!finished) {
             // Step 1: Build ranking with mutual information
-            auto pairSelection = metrics.SelectKPairs(weights_, featuresUsed, ascending, 0); // Get all the pairs sorted
+            pairSelection = metrics.SelectKPairs(weights_, featuresUsed, ascending, 0); // Get all the pairs sorted
             if (order_algorithm == Orders.RAND) {
                 std::shuffle(pairSelection.begin(), pairSelection.end(), g);
             }
-            //     // Remove used features
-            //     featureSelection.erase(remove_if(begin(featureSelection), end(featureSelection), [&](auto x)
-            //         { return std::find(begin(featuresUsed), end(featuresUsed), x) != end(featuresUsed);}),
-            //         end(featureSelection)
-            //     );
-            //     int k = bisection ? pow(2, tolerance) : 1;
-            //     int counter = 0; // The model counter of the current pack
-            //     VLOG_SCOPE_F(1, "counter=%d k=%d featureSelection.size: %zu", counter, k, featureSelection.size());
-            //     while (counter++ < k && featureSelection.size() > 0) {
-            //         auto feature = featureSelection[0];
-            //         featureSelection.erase(featureSelection.begin());
-            //         std::unique_ptr<Classifier> model;
-            //         model = std::make_unique<SPODE>(feature);
-            //         model->fit(dataset, features, className, states, weights_);
-            //         alpha_t = 0.0;
-            //         if (!block_update) {
-            //             auto ypred = model->predict(X_train);
-            //             // Step 3.1: Compute the classifier amout of say
-            //             std::tie(weights_, alpha_t, finished) = update_weights(y_train, ypred, weights_);
-            //         }
-            //         // Step 3.4: Store classifier and its accuracy to weigh its future vote
-            //         numItemsPack++;
-            //         featuresUsed.push_back(feature);
-            //         models.push_back(std::move(model));
-            //         significanceModels.push_back(alpha_t);
-            //         n_models++;
-            //         VLOG_SCOPE_F(2, "numItemsPack: %d n_models: %d featuresUsed: %zu", numItemsPack, n_models, featuresUsed.size());
-            //     }
-            //     if (block_update) {
-            //         std::tie(weights_, alpha_t, finished) = update_weights_block(k, y_train, weights_);
-            //     }
-            //     if (convergence && !finished) {
-            //         auto y_val_predict = predict(X_test);
-            //         double accuracy = (y_val_predict == y_test).sum().item<double>() / (double)y_test.size(0);
-            //         if (priorAccuracy == 0) {
-            //             priorAccuracy = accuracy;
-            //         } else {
-            //             improvement = accuracy - priorAccuracy;
-            //         }
-            //         if (improvement < convergence_threshold) {
-            //             VLOG_SCOPE_F(3, "  (improvement<threshold) tolerance: %d numItemsPack: %d improvement: %f prior: %f current: %f", tolerance, numItemsPack, improvement, priorAccuracy, accuracy);
-            //             tolerance++;
-            //         } else {
-            //             VLOG_SCOPE_F(3, "* (improvement>=threshold) Reset. tolerance: %d numItemsPack: %d improvement: %f prior: %f current: %f", tolerance, numItemsPack, improvement, priorAccuracy, accuracy);
-            //             tolerance = 0; // Reset the counter if the model performs better
-            //             numItemsPack = 0;
-            //         }
-            //         if (convergence_best) {
-            //             // Keep the best accuracy until now as the prior accuracy
-            //             priorAccuracy = std::max(accuracy, priorAccuracy);
-            //         } else {
-            //             // Keep the last accuray obtained as the prior accuracy
-            //             priorAccuracy = accuracy;
-            //         }
-            //     }
-            //     VLOG_SCOPE_F(1, "tolerance: %d featuresUsed.size: %zu features.size: %zu", tolerance, featuresUsed.size(), features.size());
-            //     finished = finished || tolerance > maxTolerance || featuresUsed.size() == features.size();
+            int k = bisection ? pow(2, tolerance) : 1;
+            int counter = 0; // The model counter of the current pack
+            // VLOG_SCOPE_F(1, "counter=%d k=%d featureSelection.size: %zu", counter, k, featureSelection.size());
+            while (counter++ < k && pairSelection.size() > 0) {
+                auto feature_pair = pairSelection[0];
+                pairSelection.erase(pairSelection.begin());
+                std::unique_ptr<Classifier> model;
+                model = std::make_unique<SPnDE>(std::vector<int>({ feature_pair.first, feature_pair.second }));
+                model->fit(dataset, features, className, states, weights_);
+                alpha_t = 0.0;
+                if (!block_update) {
+                    auto ypred = model->predict(X_train);
+                    // Step 3.1: Compute the classifier amout of say
+                    std::tie(weights_, alpha_t, finished) = update_weights(y_train, ypred, weights_);
+                }
+                // Step 3.4: Store classifier and its accuracy to weigh its future vote
+                numItemsPack++;
+                models.push_back(std::move(model));
+                significanceModels.push_back(alpha_t);
+                n_models++;
+                // VLOG_SCOPE_F(2, "numItemsPack: %d n_models: %d featuresUsed: %zu", numItemsPack, n_models, featuresUsed.size());
+            }
+            if (block_update) {
+                std::tie(weights_, alpha_t, finished) = update_weights_block(k, y_train, weights_);
+            }
+            if (convergence && !finished) {
+                auto y_val_predict = predict(X_test);
+                double accuracy = (y_val_predict == y_test).sum().item<double>() / (double)y_test.size(0);
+                if (priorAccuracy == 0) {
+                    priorAccuracy = accuracy;
+                } else {
+                    improvement = accuracy - priorAccuracy;
+                }
+                if (improvement < convergence_threshold) {
+                    // VLOG_SCOPE_F(3, "  (improvement<threshold) tolerance: %d numItemsPack: %d improvement: %f prior: %f current: %f", tolerance, numItemsPack, improvement, priorAccuracy, accuracy);
+                    tolerance++;
+                } else {
+                    // VLOG_SCOPE_F(3, "* (improvement>=threshold) Reset. tolerance: %d numItemsPack: %d improvement: %f prior: %f current: %f", tolerance, numItemsPack, improvement, priorAccuracy, accuracy);
+                    tolerance = 0; // Reset the counter if the model performs better
+                    numItemsPack = 0;
+                }
+                if (convergence_best) {
+                    // Keep the best accuracy until now as the prior accuracy
+                    priorAccuracy = std::max(accuracy, priorAccuracy);
+                } else {
+                    // Keep the last accuray obtained as the prior accuracy
+                    priorAccuracy = accuracy;
+                }
+            }
+            // VLOG_SCOPE_F(1, "tolerance: %d featuresUsed.size: %zu features.size: %zu", tolerance, featuresUsed.size(), features.size());
+            finished = finished || tolerance > maxTolerance || pairSelection.size() == 0;
         }
-        // if (tolerance > maxTolerance) {
-        //     if (numItemsPack < n_models) {
-        //         notes.push_back("Convergence threshold reached & " + std::to_string(numItemsPack) + " models eliminated");
-        //         VLOG_SCOPE_F(4, "Convergence threshold reached & %d models eliminated of %d", numItemsPack, n_models);
-        //         for (int i = 0; i < numItemsPack; ++i) {
-        //             significanceModels.pop_back();
-        //             models.pop_back();
-        //             n_models--;
-        //         }
-        //     } else {
-        //         notes.push_back("Convergence threshold reached & 0 models eliminated");
-        //         VLOG_SCOPE_F(4, "Convergence threshold reached & 0 models eliminated n_models=%d numItemsPack=%d", n_models, numItemsPack);
-        //     }
-        // }
-        // if (featuresUsed.size() != features.size()) {
-        //     notes.push_back("Used features in train: " + std::to_string(featuresUsed.size()) + " of " + std::to_string(features.size()));
-        //     status = WARNING;
-        // }
-        // notes.push_back("Number of models: " + std::to_string(n_models));
+        if (tolerance > maxTolerance) {
+            if (numItemsPack < n_models) {
+                notes.push_back("Convergence threshold reached & " + std::to_string(numItemsPack) + " models eliminated");
+                // VLOG_SCOPE_F(4, "Convergence threshold reached & %d models eliminated of %d", numItemsPack, n_models);
+                for (int i = 0; i < numItemsPack; ++i) {
+                    significanceModels.pop_back();
+                    models.pop_back();
+                    n_models--;
+                }
+            } else {
+                notes.push_back("Convergence threshold reached & 0 models eliminated");
+                // VLOG_SCOPE_F(4, "Convergence threshold reached & 0 models eliminated n_models=%d numItemsPack=%d", n_models, numItemsPack);
+            }
+        }
+        if (pairSelection.size() > 0) {
+            notes.push_back("Used pairs not used in train: " + std::to_string(pairSelection.size()));
+            status = WARNING;
+        }
+        notes.push_back("Number of models: " + std::to_string(n_models));
     }
     std::vector<std::string> BoostA2DE::graph(const std::string& title) const
     {
