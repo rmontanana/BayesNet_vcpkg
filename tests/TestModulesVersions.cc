6fcc15d39aa312d59fbe6ef9d2b3a018c00828a5
Ricardo Montañana Gómez <rmontanana@gmail.com>
2024-06-24 12:38:44 +0200

    Upgrade mdlp library

--- tests/TestModulesVersions.cc
+++ tests/TestModulesVersions.cc
@@ -16,7 +16,7 @@
 #include "TestUtils.h"
 
 std::map<std::string, std::string> modules = {
-    { "mdlp", "1.2.1" },
+    { "mdlp", "1.2.2" },
     { "Folding", "1.1.0" },
     { "json", "3.11" },
     { "ArffFiles", "1.1.0" }
